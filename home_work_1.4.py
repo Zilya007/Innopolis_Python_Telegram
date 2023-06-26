22dbd7b5438f54dd5c49257a5694e64064563040
Zilya <ramr.f@yandex.ru>
2023-06-26 15:37:53 +0300

    предварительно рабочая версия

---
+++ home_work_1.4.py
@@ -0,0 +1,28 @@
+print("Построю елочку из Вашего символа!")
+
+flag_input = True
+while flag_input:
+    symbol = input("Введите любой один символ:\n")
+    if len(symbol) == 1:
+            level = input("Введите высоту до 30:\n")
+            if level.isnumeric() and int(level) <= 30:
+                level = int(level)
+                flag_input = False
+            else:
+                print("Ошибка Ввода")
+                flag_input = True
+    else:
+        print("Ошибка ввода!")
+        flag_input = True
+
+split = level - 1
+count = 1
+for i in range(0, level):
+    print(split * ' ', end='')
+    split -= 1
+    print(count * symbol, end='')
+    count += 2
+    print()
+
+
+
