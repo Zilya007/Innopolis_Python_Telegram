478772bdb02a23aeafdffba6937c28837c1dc944
Zilya <ramr.f@yandex.ru>
2023-07-03 09:42:18 +0300

    Рабочая версия

--- home_work_1.6.py
+++ home_work_1.6.py
@@ -1,31 +1,18 @@
 #Реализовать приложение для проверки верности введенных пользователем данных в виде строки
-word = 'Реализовать приложение для проверки верности введенных пользователем данных в виде строки'
-# print(word.split())
-# print(word.count('а'))
+
 fio = []
 while True:
-    user_name = input("Введите ФИО через пробел: ").title()
+    user_name = input("Введите ФИО через пробел:\n").title()
     fio = user_name.split()
     if len(fio) == 3:
-        for item in fio:
-            print(item,sep=' ')
-
-        number = input("Введите номер телефона без восьмерки! ").strip()
+        number = input("Введите номер телефона без восьмерки:\n").strip()
         if number.isdigit() and len(number) == 10:
             phone = number.strip()
+            print(f'Поздравляем {fio[1]}, Вы успешно зарегистрированы!')
+            print("Ваши регистрационные данные:\n Фамилия: {}\n Имя: {}\n "
+                  "Отчество: {}\n Номер телефона: {}".format(fio[0], fio[1], fio[2], phone))
+            break
         else:
             print(f'Ошибка ввода, Вы ввели {number}')
-
-        print(f'Поздравляем {fio[1]}, Вы успешно зарегистрированы.')
-        print(сделать с форматом рег данные)
-
     else:
         print(f"Ошибка ввода, Вы ввели {user_name}")
-
-
-#Фатыхов Рамиль Ильдусович 9196316800
-
-
-
-
-# print(user_name.split())
