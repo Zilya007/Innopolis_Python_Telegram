05d1b998a5dd1d1150b3a91fdbee5ffd6a3c1f13
Zilya <ramr.f@yandex.ru>
2023-07-05 16:21:11 +0300

    Занятие 1.7-2

---
+++ training_collections_dicts.py
@@ -0,0 +1,37 @@
+#a = {'a': 1, 'b': 2}
+#print(a['a'])
+#print(len(a))
+#b = {'Иванов': {'Иван': [1, 2, 3]}}
+# b = a.copy()
+# print(b)
+# print(a.pop('a'))
+#print(a.keys())
+#a.update({'c': 3})
+# a.update(d=4)       аргументы kwargs
+# a['e'] = 5
+# print(a.fromkeys({'a','r'}))
+# print(a.setdefault('c', 3)) Позволяет получить значение ключа или создать новый ключ со значением
+# a.popitem()
+#print(a.values())
+#print(a.get('a'))  Получение значения
+# if a.get('c') is None:
+#     print('No')
+# else:
+#     print('Yes')
+#b = list(a.items())
+#for elem in b:
+ #   print(elem[0])
+
+a = {'Директор': {'Иванов': {'Иван': ['35 лет', 'женат'] }  }, 'Зам. директора' : {} }
+# b = a['Директор']['Иванов']['Иван']
+# c, d = b
+# print(c)
+# print(d)
+
+#a['Директор']['Иванов']['Иван'] = ['36 лет','не женат']
+
+#a = 'ljfdskdfjsfffffffffffffslk' \
+#    'fjdskljflkjljflfjskldjlfj'               перенос строки
+
+
+print(a)
