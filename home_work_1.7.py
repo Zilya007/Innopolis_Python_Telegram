2fc477e4598f6eb05a49f86c71c21f518e1c331c
Zilya <ramr.f@yandex.ru>
2023-07-07 14:09:53 +0300

    рабочая версия

--- home_work_1.7.py
+++ home_work_1.7.py
@@ -1,42 +1,16 @@
 # Реализовать поиск последовательности Фиббоначи, А также найти элементы повторяющиеся в коллекции
 
-# fib = [0, 1]
-#  for i in range(2,100):
-#      fib.append(fib[i-1] + fib[i-2])
-#  print(fib)
-#
-#  count = 0
-#  while len(fib) <= 100:
-#      fib.append(fib[count] + fib[count + 1])
-#      count += 1
-#  print(fib)
-#
-# count = 0
-# while fib[-1] < 100:
-#     fib.append(fib[count] + fib[count + 1])
-#     count += 1
-# print(fib)
+collection = ['hello', 0, 1, 1, 2, 3, 3, 5, 8, 8, 8, ['a', ['b']], 13, 21, 34, 55, 89, 89, 89, 144, 233, 377, 610, 'hello']
+base_collection = collection.copy()
 
-fib2 = [0, 1]
-count = 0
-while len(fib2) <= 15:
-    fib2.append(fib2[count] + fib2[count + 1])
-    count += 1
-print(fib2)
-
-
-#collection = ['hello', 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
-collection = ['hello', 0, 1, 1, 2, 3, 3, 5, 8, 8, 8, ['a', ['b']], 13, 21, 34, 55, 89, 89, 89, 144, 233, 377, 610]
-#collection = fib2.copy()
-fib = []
-reps = []
+fib = []  # для генерации ряда фиббоначи
+reps = []  # для отслеживания повторений
 
 for elem in collection:
     if collection.count(elem) > 1 and elem not in reps:
         reps.append(elem)
-print(f' Повторяющиеся элементы в последовательности : {reps}')
 
-deleted = []
+deleted = []  # список для отслеживания удаленных элементов из списка
 
 for elem in collection:
     if isinstance(elem, int) and elem not in deleted:
@@ -44,10 +18,9 @@ for elem in collection:
     else:
         deleted.append(elem)
 
-for elem in collection:
+for elem in collection:  # удаление нечисловых элементов последовательности
     if elem in deleted:
         collection.remove(elem)
-print(collection)
 
 for i in range(len(collection)):
     if collection[i] == collection[i - 2] + collection[i - 1]:
@@ -68,6 +41,8 @@ for i in range(len(collection)):
         pass
 
 if len(fib) > 0:
-    print(fib)
+    print('В последовательности', *base_collection)
+    print('Найден следующий ряд Фиббоначи: ', *fib)
+    print('Повторяющиеся элементы в последовательности: ', *reps )
 else:
     print("Последовательность фиббоначи не обнаружена")
