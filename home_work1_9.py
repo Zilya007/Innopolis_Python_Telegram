32a251c2db4751c7e50eb6b54a5695537d8c4a05
Zilya <ramr.f@yandex.ru>
2023-07-11 22:18:00 +0300

    макет

--- home_work1_9.py
+++ home_work1_9.py
@@ -1,3 +1,31 @@
 # Реализация приложения, которое считывает данные с файла, и сохраняет рассчитанное значение в файл на ПК пользователя
 # Планируемый результат:Реализовано приложение для обработки текстовой информации полученной с файла пользователя и
 # сохраняющее результат в файловой системе ОС пользователя
+# Программа считывает данные из файла user_file и помещает в файл config.yml в соответствующем порядке
+
+import yaml
+
+for_yaml = {}
+with open('homework1_9/user_file.txt', mode='r') as file:
+    reader = file.read()
+reader = reader.replace(',', ' ')
+reader = reader.replace('  ', ' ')
+reader = reader.replace("'", '')
+reader = reader.rstrip()
+reader = reader.split(' ')
+print(reader)
+
+for i in range(0, len(reader), 2):
+    if i == len(reader) - 1:
+        break
+    else:
+        for_yaml[reader[i]] = reader[i + 1]
+# for key,value in for_yaml.items():
+#     print(key)
+#     print(value)
+
+
+print(for_yaml)
+
+with open('homework1_9/config.yml', 'w') as file:
+    yaml.dump(for_yaml, file)
