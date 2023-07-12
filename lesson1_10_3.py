6bc6e0b739ce240400dc9fba4e18e4b33d7259ca
Zilya <ramr.f@yandex.ru>
2023-07-12 22:46:20 +0300

    лекция 1_10_3 классы

---
+++ lesson1_10_3.py
@@ -0,0 +1,54 @@
+#
+# try:
+#     num = int(input('Введите число'))
+#     print(num + 10)
+# except Exception as err:
+#     print(err)
+# finally:
+#     print('finally')
+#
+
+# class MyException(Exception):
+#     def __init__(self,*args):
+#         if args:
+#             self.message = args[0]
+#         else:
+#             self.message = None
+#
+#     def __str__(self):
+#         print("Call str")
+#         if self.message:
+#             return f'new error'
+#         else:
+#             return 'exception new error'
+#
+# raise MyException('This is my error')
+
+# class Test:
+#     def __init__(self):
+#         self.name = 4
+#
+#     def __del__(self):                  #вызывается в конце , очищает из памяти
+#         print("delete")
+#
+# a = Test()
+# print(a.name)
+
+
+class Test:
+    def __init__(self, number):
+        self.number = number
+
+    def __add__(self, other):
+        return self.number + other
+
+    def __radd__(self, other):
+        return other + self.number
+
+    def __iadd__(self, other):
+        return self.number + other
+
+
+a = Test(5)
+print(a + 10)
+print(10 + a)
