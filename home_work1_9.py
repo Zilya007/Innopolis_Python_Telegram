5b68340f51fb10197d9281324a9591182718a78a
Zilya <ramr.f@yandex.ru>
2023-07-12 12:53:39 +0300

    предварительно рабочая версия

--- home_work1_9.py
+++ home_work1_9.py
@@ -1,11 +1,13 @@
 # Реализация приложения, которое считывает данные с файла, и сохраняет рассчитанное значение в файл на ПК пользователя
 # Планируемый результат:Реализовано приложение для обработки текстовой информации полученной с файла пользователя и
 # сохраняющее результат в файловой системе ОС пользователя
-# Программа считывает данные из файла user_file и помещает в файл config.yml в соответствующем порядке
+# Программа считывает данные из файла user_file.txt и помещает в файл config.yml в соответствующем порядке
+# Дополнительно установлена библиотека pyyaml
 
 import yaml
+import os
 
-for_yaml = {}
+to_yaml = {}
 with open('homework1_9/user_file.txt', mode='r') as file:
     reader = file.read()
 reader = reader.replace(',', ' ')
@@ -13,19 +15,15 @@ reader = reader.replace('  ', ' ')
 reader = reader.replace("'", '')
 reader = reader.rstrip()
 reader = reader.split(' ')
-print(reader)
 
 for i in range(0, len(reader), 2):
     if i == len(reader) - 1:
         break
     else:
-        for_yaml[reader[i]] = reader[i + 1]
-# for key,value in for_yaml.items():
-#     print(key)
-#     print(value)
-
-
-print(for_yaml)
+        to_yaml[reader[i]] = reader[i + 1]
 
 with open('homework1_9/config.yml', 'w') as file:
-    yaml.dump(for_yaml, file)
+    yaml.safe_dump(to_yaml, file, sort_keys=False)
+
+path = os.path.abspath('homework1_9/config.yml')
+print(f'Программа завершила работу,результат сохранен в файле по адресу: {path}')
