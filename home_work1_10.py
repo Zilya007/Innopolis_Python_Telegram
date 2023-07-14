91c7921af43653e39f9d84df711f2eabac9db351
Zilya <ramr.f@yandex.ru>
2023-07-14 13:37:57 +0300

    предварительно рабочая версия

--- home_work1_10.py
+++ home_work1_10.py
@@ -2,6 +2,8 @@
 # Корректно реализован класс, использующий псевдо частные методы и реализующий перегрузку методов
 
 import random
+
+
 class Phone:
     def __init__(self, maker, model):
         self.maker = maker
@@ -11,24 +13,20 @@ class Phone:
         number = input("Наберите номер телефона! ")
         print(f'call to {number}')
 
-    def answer(self, number):
-        self.number = random.randint(8917000000,89174444444)
+    def answer(self):
         print("Ответить Да / Нет")
         choice = input()
         if choice == 'Да':
-            print(f"Вы ответили на звонок {self.number}")
+            print(f"Вы ответили на звонок")
         else:
             pass
 
 
 class IpPhone(Phone):
 
-    def __init__(self, maker, model, poe):
-        self.maker= maker
-        self.model = model
+    def __init__(self, maker, model, poe=None):
+        super().__init__(maker, model)
         self.poe = poe
-        # super().__init__(maker)
-        # super().__init__(model)
         self.history = []
 
     def call(self):
@@ -36,26 +34,94 @@ class IpPhone(Phone):
         print(f'call to {number}')
         self.history.append(number)
 
+    def answer(self):
+        self.number = random.randint(9111111111, 9999999999)
+        print("Ответить Да / Нет")
+        choice = input()
+        if choice == 'Да':
+            print(f"Вы ответили на звонок {self.number}")
+            self.history.append(self.number)
+        else:
+            pass
+
     def show_history(self):
         print("история звонков: ")
         for number in self.history:
-            print(number)
+            if self.history.index(number) == (int(len(self.history)) - 1):
+                print(number, end='.\n')
+            else:
+                print(number, end=', ')
 
 
 class ConferencePhone(IpPhone):
-    pass
+    conference_number = 0
+
+    def _create_conference(self):
+        while True:
+            members = input("Введите количество участников: ")
+            if members.isnumeric() and int(members) <= 10:
+                print(f"Создана конференция с количеством участников -  {members}.")
+                self.conference_number += 1
+                break
+            else:
+                print('Введите корректное значение')
+
+    def _show_conference_number(self):
+        print(f'Количество проведенных конференций - {self.conference_number}')
 
 
-
-
-phone1 = Phone('panasonic', 't20')
-print(phone1.maker)
-print(phone1.model)
-ip_phone1 = IpPhone('Yealink', 'T21P', 'POE')
+phone1 = Phone('Panasonic', 'XR24T')
+ip_phone1 = IpPhone('Yealink', 'T21P', 'poe')
+conference_phone1 = ConferencePhone('Yealink', 'T54W', 'poe')
 
 while True:
+    choice_phone = input(
+        "Выберите телефон: 1 - аналоговый , 2 - ip-телефон, 3- конференц-телефон, 0 - выход из программы\n")
+    match choice_phone:
+        case '1':
+            print(f"Вы используете телефон {phone1.maker} {phone1.model}")
+        case '2':
+            print(f"Вы используете телефон {ip_phone1.maker} {ip_phone1.model}")
+            while True:
+                print(
+                    "Выберите действие: 1 - Позвонить , 2 Ответить на звонок , 3 - Показать историю звонков , 0 - выход")
+                choice = input()
+                match choice:
+                    case '1':
+                        ip_phone1.call()
+                    case '2':
+                        ip_phone1.answer()
+                    case '3':
+                        ip_phone1.show_history()
+                    case '0':
+                        break
+                    case _:
+                        print("Ошибка ввода повторите")
+        case '3':
+            print(f"Вы используете телефон {conference_phone1.maker} {conference_phone1.model}")
+            while True:
+                print(
+                    "Выберите действие: 1 - Позвонить , 2 Ответить на звонок , 3 - Показать историю звонков , "
+                    "4- создать конференцию, 5- показать количество конференций 0 - выход")
+                choice = input()
+                match choice:
+                    case '1':
+                        conference_phone1.call()
+                    case '2':
+                        conference_phone1.answer()
+                    case '3':
+                        conference_phone1.show_history()
+                    case '4':
+                        conference_phone1._create_conference()
+                    case '5':
+                        conference_phone1._show_conference_number()
+                    case '0':
+                        break
+                    case _:
+                        print("Ошибка ввода повторите")
 
-    ip_phone1.call()
-    ip_phone1.show_history()
-    ip_phone1.answer(89174444444)
+        case '0':
+            break
 
+        case _:
+            print("Ошибка ввода")
