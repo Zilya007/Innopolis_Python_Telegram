cc98cc878288d6be15c348dad08a000ea693aec8
Zilya <ramr.f@yandex.ru>
2023-07-16 16:29:29 +0300

    Лекция 2 Модули

---
+++ lesson2_1.py
@@ -0,0 +1,11 @@
+# import os
+# from dotenv import load_dotenv
+#
+# load_dotenv()
+#
+# password = os.getenv('PASSWORD')
+# username = os.getenv('USER_NAME')
+#
+# print(username, password, sep=':')
+#
+
