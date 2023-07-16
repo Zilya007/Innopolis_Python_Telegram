e7e1bfa769c94d00eadce85a9e4e5415df0c6825
Zilya <ramr.f@yandex.ru>
2023-07-16 18:45:33 +0300

    Предварительно рабочая версия

---
+++ home_work2_1.py
@@ -0,0 +1,16 @@
+import module_simple_numbers
+from module_simple_numbers import simple_numbers
+
+while True:
+    choice = input("Программа вычисляет простые числа из выбранного диапазона!\n"
+              "Для продолжения наберите любой символ Выход - 0")
+    if choice != '0':
+        start = input("Введите начальное значения диапазона:\n")
+        final = input("Введите конечное значения диапазона:\n")
+        if start.isnumeric() and final.isnumeric() and int(start) < int(final):
+            simple_numbers(int(start), int(final))
+        else:
+            print("Ошибка ввода")
+    else:
+        print('Завершение работы!')
+        break
---
+++ module_simple_numbers.py
@@ -0,0 +1,24 @@
+def simple_numbers(a, b):
+    non_simple = []
+    simple = []
+    for i in range(2, b + 1):
+        for j in range(2, b + 1):
+            if i % j == 0 and i != j and i not in non_simple:
+                non_simple.append(i)
+    non_simple = list(set(non_simple))
+
+    for i in range(a, b + 1):
+        if i not in non_simple:
+            simple.append(i)
+
+    print(f'Список простых чисел диапазона {a, b}:')
+    for elem in simple:
+        if len(simple) > 6:
+            if simple.index(elem) > 0 and simple.index(elem) % 6 == 0:
+                print(elem)
+            else:
+                print(elem, end='\t')
+        else:
+            print(elem, end='\t')
+    print()
+    return simple
