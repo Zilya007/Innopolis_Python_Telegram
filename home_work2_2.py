6a9a4609e17a8df839a0106dbc18f3b2ae6230cf
Zilya <ramr.f@yandex.ru>
2023-07-21 17:00:09 +0300

    start

--- home_work2_2.py
+++ home_work2_2.py
@@ -1,4 +1,21 @@
-numbers = iter(range(1, 1000001))
-print(2000 in numbers)
-print(next(numbers) == 2001)
+# Реализовать программу использующую генераторы, и обрабатывающую данные последовательно с использованием итераторов
+
+# numbers = iter(range(1, 1000001))
+# print(2000 in numbers)
+# print(next(numbers) == 2001)
+#
+
+class Stack():
+
+    def create_stack(self):
+        stack = [i for i in range(1, 11)]
+        index = len(stack) - 1
+        for index in stack:
+            stack[index].
+            print(*stack)
+
+
+x = Stack()
+x.create_stack()
+
 
