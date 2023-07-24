7396e49232cbcf35d3e88c9beeeabf5206817185
Zilya <ramr.f@yandex.ru>
2023-07-24 16:52:58 +0300

    start

--- home_work2_2.py
+++ home_work2_2.py
@@ -4,7 +4,7 @@ class Stack():
         self.stack = [i for i in range(1, 50) if i % 2 == 0]  # генератор
         self.stack_pointer = 0
 
-    def __iter__(self):
+    def __iter__(self):                                     # настраиваем экземпляры класса итерируемыми
         stack_iter = iter(reversed(self.stack))
         return stack_iter
 
@@ -35,18 +35,26 @@ class Stack():
         else:
             print('Ошибка, стек пуст!')
 
-
 stack = Stack()
-stack.show_stack()
-# stack.peek()
-# stack.pop()
-# stack.show_stack()
-# stack.peek()
-# stack.push(100)
-# stack.show_stack()
-# stack.peek()
 myiter = iter(stack)
-# for x in myiter:
-#     print(x,end=' ')
-print(next(myiter))
-print(next(myiter))
+while True:
+    choice = input('1 - Показать стек\t\t 2 - Добавить элемент\n'
+                   '3 - Удалить элемент\t\t 4 - Показать вершину стека\n'
+                   '0 - Выход\n')
+
+
+    match choice:
+        case '1':
+            for i in myiter:
+                print(i, end=' ')
+            print()
+        case '2':
+            pass
+        case '3':
+            pass
+
+        case '4':
+            pass
+        case '0':
+            pass
+
