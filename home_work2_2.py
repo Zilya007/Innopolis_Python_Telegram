7f17f49d930f88122368cead17463472700e623b
Zilya <ramr.f@yandex.ru>
2023-07-25 12:00:53 +0300

    Предварительно рабочая версия

--- home_work2_2.py
+++ home_work2_2.py
@@ -1,17 +1,21 @@
 # Реализовать программу использующую генераторы, и обрабатывающую данные последовательно с использованием итераторов
-class Stack():
+class Stack:
     def __init__(self):
-        self.stack = [i for i in range(1, 50) if i % 2 == 0]  # генератор
-        self.stack_pointer = 0
+        self.stack = [i for i in range(1, 15) if i % 2 == 0]  # создание списка с помощью генератор
+        self.stack_pointer = (int(len(self.stack) - 1))
+        self.a = 1
 
-    def __iter__(self):                                     # настраиваем экземпляры класса итерируемыми
+    def __iter__(self):  # настраиваем экземпляры класса итерируемыми
         stack_iter = iter(reversed(self.stack))
         return stack_iter
 
+    def __len__(self):
+        return len(self.stack)
+
     def show_stack(self):
-        rev = reversed(self.stack)
+        # rev = reversed(self.stack)
         if len(self.stack) != 0:
-            print(*rev)
+            print(next(self.stack))  # использование метода next
         else:
             print('Стек пуст!')
 
@@ -21,9 +25,9 @@ class Stack():
         self.stack_pointer += 1
 
     def pop(self):
-        print("Выполняется удаление элемента из стека: ")
         if len(self.stack) != 0:
-            elem = self.stack[self.stack_pointer - 1]
+            elem = self.stack[self.stack_pointer]
+            print(f'Элемент {elem} будет удален из стека ')
             self.stack.remove(elem)
             self.stack_pointer -= 1
         else:
@@ -31,30 +35,38 @@ class Stack():
 
     def peek(self):
         if len(self.stack) != 0:
-            print(f'Элемент на вершине стека {self.stack[self.stack_pointer - 1]}')
+            print(f'Элемент на вершине стека {self.stack[self.stack_pointer]}')
         else:
             print('Ошибка, стек пуст!')
 
+
 stack = Stack()
-myiter = iter(stack)
+
 while True:
     choice = input('1 - Показать стек\t\t 2 - Добавить элемент\n'
                    '3 - Удалить элемент\t\t 4 - Показать вершину стека\n'
-                   '0 - Выход\n')
-
+                   '5 - перебор элементов\t 0 - Выход\n')
 
     match choice:
         case '1':
+            myiter = iter(stack)  # Использование итератора
             for i in myiter:
                 print(i, end=' ')
             print()
         case '2':
-            pass
+            choice = input('Введите элемент для добавления в стек: ')
+            stack.push(choice)
         case '3':
-            pass
-
+            stack.pop()
         case '4':
-            pass
+            stack.peek()
+        case '5':
+            myiter = iter(stack)
+            input("Нажимайте 'Enter' для последовательного перебора элементов  ")
+            for i in range(len(stack)):
+                print(next(myiter), end=' ')  # использование итератора
+                input()
         case '0':
+            print('Завершение работы')
+            break
             pass
-
